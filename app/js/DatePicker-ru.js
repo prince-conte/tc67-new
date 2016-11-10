c3d677170fb312b3a6aec28dd8ee25e539ae80cc
francesco <prince-conte@outlook.com>
2016-11-10 17:39:52 +0300

    Правки + календарь ЛК

---
+++ app/js/DatePicker-ru.js
@@ -0,0 +1,37 @@
+/* Russian (UTF-8) initialisation for the jQuery UI date picker plugin. */
+/* Written by Andrew Stromnov (stromnov@gmail.com). */
+( function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define( [ "../widgets/datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+}( function( datepicker ) {
+
+datepicker.regional.ru = {
+	closeText: "Закрыть",
+	prevText: "Пред",
+	nextText: "След",
+	currentText: "Сегодня",
+	monthNames: [ "<span>Январь</span>","<span>Февраль</span>","<span>Март</span>","<span>Апрель</span>","<span>Май</span>","<span>Июнь</span>",
+	"<span>Июль</span>","<span>Август</span>","<span>Сентябрь</span>","<span>Октябрь</span>","<span>Ноябрь</span>","<span>Декабрь</span>" ],
+	monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+	"Июл","Авг","Сен","Окт","Ноя","Дек" ],
+	dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+	dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+	dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+	weekHeader: "Нед",
+	dateFormat: "dd.mm.yy",
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: "" };
+datepicker.setDefaults( datepicker.regional.ru );
+
+return datepicker.regional.ru;
+
+} ) );
